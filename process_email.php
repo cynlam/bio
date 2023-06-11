6d02b1de5cf62944e676741c2f83729b83448d49
Cynthia Lam <57033243+cynlam@users.noreply.github.com>
2023-06-11 22:31:17 +1000

    Add files via upload

---
+++ process_email.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $lowercaseEmail = strtolower($email);
+    $keywords = array("suncorp", "allianz", "iconic", "cyn");
+    $hasKeyword = false;
+
+    // Check if the email contains any of the keywords
+    foreach ($keywords as $keyword) {
+        if (stripos($lowercaseEmail, $keyword) !== false) {
+            $hasKeyword = true;
+            break;
+        }
+    }
+
+    // Store the email in a text file
+
+// Generate a verification token
+$token = bin2hex(random_bytes(16));
+
+// Store the email and token in a text file or database
+file_put_contents("emails.txt", $email . ":" . $token . PHP_EOL, FILE_APPEND);
+
+    // Redirect based on conditions
+    if ($hasKeyword) {
+        header("Location: cyncv.html?email=" . urlencode($email) . "&token=" . urlencode($token));
+    } else {
+        header("Location: noaccess.html");
+    }
+}
+?>
